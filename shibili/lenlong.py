50585bbcb11c74b412a1bfb7de4d296bd4c1cb5d
shibili <muhammedshibilm987@gmail.com>
2023-07-18 08:36:40 +0530

    adding

--- shibili/lenlong.py
+++ shibili/lenlong.py
@@ -1,18 +1,12 @@
-list=["apple","pappaya","grape"]
+list=["apple","kiwi","pappaya"]
 list2=[]
 for i in list:
     list2.append(i)
-    list2.sort()
-   
-    
+    print(i)
+    list2.reverse()
 print(list2[0])
 
-#     c=(len(i))
-#     print(c)
-   
-#     list2.sort()
-# print(list2)
 
-    
-    # list2.append(c)
-    # print(c)
+
+
+
---
+++ shibili/sumducelmn.py
@@ -0,0 +1,10 @@
+number =	{
+  "num1": 1,
+  "num2": 10,
+  "num3": 3
+}
+sum=0
+for x in number.values():
+  
+ sum=sum+x
+print(sum)
